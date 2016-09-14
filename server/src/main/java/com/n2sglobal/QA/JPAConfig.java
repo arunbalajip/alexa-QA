30a609c5b679a3ed5428b55d01d49349857441b6
arunbalaji prithiviraj <arunbalaji prithiviraj>
2016-09-13 21:46:49 -0400

    feat(.gitignore): update .gitignore

---
+++ server/src/main/java/com/n2sglobal/QA/JPAConfig.java
@@ -0,0 +1,75 @@
+package com.n2sglobal.QA;
+
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan
+public class JPAConfig {
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setDataSource(dataSource());
+		String[] packageToScan  = new String[2];
+		packageToScan[0] = "com.n2sglobal.QA.topic.entity";
+		packageToScan[1] = "com.n2sglobal.QA.user.entity";
+		emf.setPackagesToScan(packageToScan);
+		JpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		emf.setJpaVendorAdapter(adapter);
+		emf.setJpaProperties(properties());
+		return emf;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		ComboPooledDataSource ds = new ComboPooledDataSource();
+		try {
+			ds.setDriverClass("com.mysql.jdbc.Driver");
+		
+		ds.setJdbcUrl("jdbc:mysql://aavocs98v6gu8v.cxv3d3t9gkac.us-west-2.rds.amazonaws.com:3306/ebdb");
+		ds.setUser("nemi");
+		ds.setPassword("n2sglobal");
+		ds.setAutoCommitOnClose(true);
+		ds.setIdleConnectionTestPeriod(15);
+		ds.setMaxIdleTime(15);
+
+		} catch (PropertyVetoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ds;
+	}
+
+	@Bean
+	public PlatformTransactionManager txnManager(EntityManagerFactory emf) {
+		JpaTransactionManager txnMgr = new JpaTransactionManager(emf);
+		return txnMgr;
+	}
+
+	private Properties properties() {
+		Properties props = new Properties();
+		props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		props.setProperty("hibernate.hbm2ddl.auto", "update");
+		props.setProperty("hibernate.show_sql", "true");
+		props.setProperty("hibernate.format_sql", "true");
+		return props;
+	}
+}

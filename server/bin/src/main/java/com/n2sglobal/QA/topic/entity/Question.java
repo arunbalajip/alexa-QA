e687f2ac5078065f30f0a535e59a217ef3e3c165
Kumaraguru <gurukarbonn@gmail.com>
2016-09-17 13:16:44 -0400

    QuzAnz - Test Web Service Changes

---
+++ server/bin/src/main/java/com/n2sglobal/QA/topic/entity/Question.java
@@ -0,0 +1,72 @@
+package com.n2sglobal.QA.topic.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="Question_List")
+public class Question {
+	@Id
+	@GenericGenerator(name = "customUUID", strategy = "uuid2")
+	@GeneratedValue(generator = "customUUID")
+	private String id;
+	@Column(unique = true)
+	String question;
+	String option1;
+	String option2;
+	String option3;
+	String answer;
+	@OneToOne
+	Topic topic;
+	
+	public Topic getTopic() {
+		return topic;
+	}
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getOption1() {
+		return option1;
+	}
+	public void setOption1(String option1) {
+		this.option1 = option1;
+	}
+	public String getOption2() {
+		return option2;
+	}
+	public void setOption2(String option2) {
+		this.option2 = option2;
+	}
+	public String getOption3() {
+		return option3;
+	}
+	public void setOption3(String option3) {
+		this.option3 = option3;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+}
---
+++ server/bin/src/main/java/com/n2sglobal/QA/topic/entity/Topic.java
@@ -0,0 +1,49 @@
+package com.n2sglobal.QA.topic.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="Topic_List")
+@NamedQueries({
+	@NamedQuery(name = "Topic.findAll", query = "SELECT m FROM Topic m"),
+		@NamedQuery(name = "Topic.findByTopic", query = "SELECT m FROM Topic m WHERE m.topic=:mtopic") 
+	})
+
+public class Topic {
+	@Id
+	@GenericGenerator(name = "customUUID", strategy = "uuid2")
+	@GeneratedValue(generator = "customUUID")
+	private String id;
+	private String topic;
+	/*@OneToMany(cascade = { CascadeType.ALL })
+	List<Question> questions;*/
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTopic() {
+		return topic;
+	}
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+	/*public List<Question> getQuestions() {
+		return questions;
+	}
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}*/
+}
